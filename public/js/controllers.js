8fca5f7f4a4d66f0177e91ac499c28aed75f56ff
arvindc95 <arvind_95@hotmail.com>
2016-07-13 06:30:45 -0400

    built web app

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,7 +2,49 @@
 
 /* Controller */
 angular.module('myApp.controllers', []).
-	controller('AppCtrl', function ($scope, $http) {
-		// Insert controller code here
-		
-	});
+    controller('AppCtrl', function ($scope, $http) {
+        // Insert controller code here
+        $scope.init = function() {
+            $http({
+              method: 'GET',
+              url: '/api/apparel'
+            }).then(function successCallback(response) {
+                $scope.styleCodeList = response.data;
+              }, function errorCallback(response) {
+                console.log(response);
+              });
+        };
+
+        $scope.getApparelData = function() {
+            $scope.chosenStyleCode = JSON.parse($scope.chosenStyleCode);
+            $http({
+              method: 'GET',
+              url: '/api/apparel/' + $scope.chosenStyleCode.style_code
+            }).then(function successCallback(response) {
+                $scope.apparelData = response.data;
+              }, function errorCallback(response) {
+                console.log(response);
+              });
+        };
+
+        $scope.getQuote = function() {
+            $http({
+              method: 'POST',
+              data: {styleCode: $scope.chosenStyleCode.style_code, 
+                    colorCode: $scope.chosenColorCode, sizeCode: $scope.chosenSizeCode, 
+                    quantity: $scope.chosenQuantity, weight: $scope.chosenStyleCode.weight},
+              url: '/api/quote'
+            }).then(function successCallback(response) {
+                if (isNaN(response.data)) {
+                    $scope.price = 'Not in stock';
+                    $scope.pricePerUnit = 'Not applicable';
+                } else {
+                    $scope.price = '$' + response.data;
+                    $scope.pricePerUnit = '$' + Math.round(response.data / $scope.chosenQuantity * 100) / 100;
+                }
+              }, function errorCallback(response) {
+                console.log(response);
+              });
+        };
+
+    });
--- routes/api.js
+++ routes/api.js
@@ -7,34 +7,128 @@ var q           = require('q');
 var api         = express.Router();
 var db          = require('../config/db').connection;
 
-// API endpoint for /api/apparel
-api.get('/api/apparel/:styleCode?', function(req, res) {
-	// Insert Apparel API code here
+api.get('/api/apparel', function(req, res) {
+    db.query('SELECT style_code, weight FROM apparel', function(err, rows, fields) {
+        if (err) throw err;
+        res.send(rows);
+    });
+});
 
+// API endpoint for /api/apparel
+api.get('/api/apparel/:styleCode', function(req, res) {
+    // Insert Apparel API code here
+
+    db.query('SELECT * FROM apparel WHERE style_code = ?', [req.params.styleCode], function(err, rows, fields) {
+        if (err) throw err;
+
+        var value = {};
+
+        value.styleCode = req.params.styleCode;
+        value.colorCodes = [];
+        value.sizeCodes = [];
+
+
+        var sizeCodes = rows[0].size_codes.split(';');
+
+        for (var i=0; i<sizeCodes.length; i++) {
+            value.sizeCodes.push({
+                code: sizeCodes[i].split(':')[0],
+                value: sizeCodes[i].split(':')[1]
+            });
+        }
+
+        var colorCodes = rows[0].color_codes.split(';');
+
+        for (var i=0; i<colorCodes.length; i++) {
+            value.colorCodes.push({
+                code: colorCodes[i].split(':')[0],
+                hex: colorCodes[i].split(':')[1],
+                name: colorCodes[i].split(':')[2]
+            });
+        }
+
+        res.send(value);
+    });
 });
 
 // API endpoint for /api/quote
 api.post('/api/quote', function(req, res) {
-	// Insert Quoting API code here
-
+    // Insert Quoting API code here
+    getApparelPrice(req.body.styleCode, req.body.colorCode, req.body.sizeCode, req.body.quantity, req.body.weight).then(function(price) {
+        res.send(price.toString());
+    });
 });
 
 // Function for making an Inventory API call
-var getApparelPrice = function getPrice(style_code, color_code, size_code) {
-	var	apparelPriceDeferred = q.defer();
+var getApparelPrice = function getPrice(style_code, color_code, size_code, quantity, weight) {
+    var    apparelPriceDeferred = q.defer();
+    // Format the Inventory API endpoint as explained in the documentation
+    https.get('https://www.alphashirt.com/cgi-bin/online/xml/inv-request.w?sr=' + style_code +'&cc=' + color_code + '&sc=' + size_code + '&pr=y&zp=20176&userName=triggered1111&password=triggered2222', function(res) {
+        res.on('data', function (data) {
+            // Parse response XML data here
+            var xml = data.toString();
+            var item = xml.substring(xml.indexOf('<item') + '<item '.length, xml.indexOf('>', xml.indexOf('<item')));
+            var needed = [
+                { xml: 'price', json: 'price' },
+                { xml: 'special-price', json: 'specialPrice' },
+                { xml: 'special-expiry', json: 'specialExpiry' }
+            ];
+            var values = {};
 
-	// Format the Inventory API endpoint as explained in the documentation
-	https.get('// INSERT INVENTORY API END POINT', function(res) {
-		res.on('data', function (data) {
-			// Parse response XML data here
 
-		});
-	}).on('error', function(error) {
-		// Handle EDI call errors here
+            for (var i=0; i<needed.length; i++) {
+                var value = item.substring(item.indexOf(needed[i].xml + '="') + needed[i].xml.length + 2, item.indexOf('"', item.indexOf(needed[i].xml + '="') + needed[i].xml.length + 3));
+                
+                if (value.indexOf('$') !== -1) {
+                    value = parseFloat(value.replace('$', ''));
+                } else if (value.indexOf('/')) {
+                    value = Date.parse(value);
+                }
 
-	});
+                values[needed[i].json] = value;
+            }
 
-	return apparelPriceDeferred.promise;
+
+            var price = values.specialExpiry > new Date().getTime() ? values.specialPrice : values.price;
+
+            console.log(price);
+
+            if (weight <= 0.4) {
+                if (quantity < 48) {
+                    price += quantity;
+                } else {
+                    price += 0.75 * quantity;
+                }
+            } else {
+                if (quantity < 48) {
+                    price += 0.5 * quantity;
+                } else {
+                    price += 0.25 * quantity;
+                }
+            }
+            
+            console.log(price);
+
+            price *= 1.07;
+
+            console.log(price);
+
+            if (price <= 800) {
+                price *= 1.5;
+            } else {
+                price *= 1.45;
+            }
+
+            console.log(price);
+
+            apparelPriceDeferred.resolve(Math.round(price * 100) / 100);
+        });
+    }).on('error', function(error) {
+        // Handle EDI call errors here
+        apparelPriceDeferred.reject(new Error(error));
+    });
+
+    return apparelPriceDeferred.promise;
 }
 
 module.exports = api;
